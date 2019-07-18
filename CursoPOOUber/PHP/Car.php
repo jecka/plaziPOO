6c011e2ad222741e3bb75cc5e41a2caa729580e6
Jessica Espinosa <isc.espinosalopez@gmail.com>
2019-07-17 20:09:27 -0500

    php class

--- CursoPOOUber/PHP/Car.php
+++ CursoPOOUber/PHP/Car.php
@@ -1,7 +1,17 @@
 <?php
+require_once 'Account.php'
 class Car{
   public $id;
   public $license;
-  public $driver;
+  Account $driver;
   public $passenger;
+
+  function __construct($license , Account $driver){
+    $this->license  = $license;
+    $this->driver = $driver;
+  }
+
+  public function printDataCr(){
+     echo "License: ".$this->license . "Driver: ".$this->driver->name;
+  }
 }
