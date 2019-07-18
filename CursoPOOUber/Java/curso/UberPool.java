7b932debd255cc7dbb315d113005f0e00c7b465a
Jessica Espinosa <isc.espinosalopez@gmail.com>
2019-07-18 17:01:22 -0500

    herencia
    herencia

--- CursoPOOUber/Java/curso/UberPool.java
+++ CursoPOOUber/Java/curso/UberPool.java
@@ -1,8 +1,8 @@
-class UberX extends Car{
+class UberPool extends Car{
     String brand;
     String model;
 
-    public UberX(String license, Account driver, String brand, String model){
+    public UberPool(String license, Account driver, String brand, String model){
         super (license, driver);
         this.brand = brand;
         this.model =  model;
