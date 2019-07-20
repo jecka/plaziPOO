b85e6d020a93119dce75058333f868c4f897378c
Jessica Espinosa <isc.espinosalopez@gmail.com>
2019-07-19 19:40:56 -0500

    Update index.php
    index

--- CursoPOOUber/PHP/index.php
+++ CursoPOOUber/PHP/index.php
@@ -1,3 +1,12 @@
 <?php
+require_once("car.php");
+require_once("UberX.php");
+require_once("UberPool.php");
+require_once("Account.php");
 
+$UbeX = new UberX("CVS123", new Account("Andres Herrera","AND235","Chevrolet","Spark"));
+$UberX->printDataCar();
+
+$UberPool = new UberPool("CVS123", new Account("Andrea Ferran","AND235","Dodge","A"));
+$UberPool->printDataCar();
 ?>
