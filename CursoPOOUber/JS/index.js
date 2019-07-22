601affaa4d731963ed3e63bd8b0d20f838de86d1
Jessica Espinosa <isc.espinosalopez@gmail.com>
2019-07-22 12:54:15 -0500

    creacion Uber
    herencia uber

--- CursoPOOUber/JS/index.js
+++ CursoPOOUber/JS/index.js
@@ -1,3 +1,7 @@
 var car = new  Car("AWF456" , new Account("Andres Herrera","QWE234"))
 car.passenger=4
 car.printDataCar();
+
+var  uberX = new UberX("AWF456", new Account("Andrea Ferran", "WER789"), "Chevrolet", "Spark")
+uberX.passenger =4;
+uberX.printDataCar();
