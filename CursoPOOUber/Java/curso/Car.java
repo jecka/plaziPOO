b88394c750978eb2e038484f4331f126a4bb05d0
Jessica Espinosa <isc.espinosalopez@gmail.com>
2019-07-24 19:22:46 -0500

    encapsulamiento java

--- CursoPOOUber/Java/curso/Car.java
+++ CursoPOOUber/Java/curso/Car.java
@@ -2,7 +2,7 @@ class Car{
     Integer id;
     String license;
     Account driver;
-    Integer passegenger;
+    private Integer passenger;
 
     public Car(String license, Account driver){
          this.license = license;
@@ -10,7 +10,18 @@ class Car{
     }
 
     void printDataCar(){
-        System.out.println("License: "+ license + " Name Driver: "+ driver.name);
-    }
+        System.out.println("License: "+ license + " Name Driver: "+ driver.name + " Passenger: "+passenger);
+     }
+
+     public Integer  getPassenger(){
+         return passenger;
+     }
+
+     public void setPassenger(Integer passenger){
+         if(passenger ==4)
+            this.passenger = passenger;    
+        else
+            System.out.println("Necesitas asignar 4 pasajeros ");
+     }
 }
 
--- CursoPOOUber/Java/curso/Main.java
+++ CursoPOOUber/Java/curso/Main.java
@@ -1,15 +1,14 @@
 class Main {
     public static void main(String[] args) {
-
-        Car car = new Car("AMA123",new Account("Andres Herrera", "AND123"));
-        car.passegenger =4;
-        car.printDataCar();
+        UberX uberx = new UberX ("AMA123"  ,new Account("Andres Herrera", "AND123")  ,"Chevrolet " ,"Sonic");
+        uberx.setPassenger(4);
+        uberx.printDataCar();
        
-
+        /*
         Car car2= new Car("QWE567", new Account("Andrea Herrera", "ABC123"));
         car2.passegenger=3;
         car2.printDataCar();
-        
+     */   
     }
        
 }
